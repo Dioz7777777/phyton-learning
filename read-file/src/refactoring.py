730fc6ea5e9752b7816da98385164d54e9dff521
Vadim Litvin <vadim.litvin@compass-usa.com>
2023-02-16 09:13:57 +0100

    Week 2

--- read-file/src/refactoring.py
+++ read-file/src/refactoring.py
@@ -1,26 +1,56 @@
 import datetime
 from Student import Student
 from typing import Dict, List
+from Grade import Grade
+
+
+def safe_convert_to_float(value: str) -> float | None:
+    try:
+        return float(value)
+    except ValueError:
+        return None
 
 
 def read_grades_from_file(filename: str) -> Dict[str, Student]:
     result = {}
-    with open(filename, "r") as file:
-        lines = file.readlines()
-        for line in lines:
-            line_without_spaces = line.replace(" ", "")
-            line_values = line_without_spaces.split(",")
-            if not any(line_values):
-                print("Empty line")
-                continue
-            if len(line_values) == 1:
-                print(name + ":", "has no grades")
-                continue
-            name = line_values[0]
-            grades = line_values[1:]
-            grades_as_numbers = [float(grade) for grade in grades]
-            result[name] = Student(name, grades_as_numbers)
-    return result
+    try:
+        with open(filename, "r") as file:
+            lines = file.readlines()
+            if not lines:
+                print("File is empty")
+                return dict()
+            for line in lines:
+                line_without_spaces = line.replace(" ", "")
+                line_values = line_without_spaces.split(",")
+                if not any(line_values):
+                    print("Empty line")
+                    continue
+                if len(line_values) == 1:
+                    print(name + ":", "has no grades")
+                    continue
+                name = line_values[0]
+                grades = line_values[1:]
+                grades_as_numbers = []
+
+                for grade in grades:
+                    converted_grade = safe_convert_to_float(grade)
+                    if converted_grade is None:
+                        if grade == "":
+                            print(name + ":", "has empty grade")
+                            continue
+                        else:
+                            print(name + ":", f"Invalid grade: {grade}")
+                            continue
+                    if converted_grade < Grade.Min or converted_grade > Grade.Max:
+                        print(name + ":", f"Invalid grade: {converted_grade}")
+                        continue
+                    grades_as_numbers.append(converted_grade)
+
+                result[name] = Student(name, grades_as_numbers)
+        return result
+    except FileNotFoundError:
+        print(f"File [{filename}] not found")
+        return result
 
 
 def show_result(students: List[Student]) -> None:
@@ -33,9 +63,6 @@ def show_result(students: List[Student]) -> None:
 
 def main(file_name: str, sort_by: str) -> None:
     grades_result = read_grades_from_file(file_name)
-    if grades_result is None:
-        print("File not found")
-        return
 
     if sort_by == "N":
         sorted_by_name = dict(sorted(grades_result.items()))
